dd0cb85dfd070eea4199b7c94e500516d86f524f
Jonatan Westholm <tpi12jwe@student.lu.se>
2017-03-23 18:07:18 +0100

    svm and aux

---
+++ subgroup_auxilliary.py
@@ -0,0 +1,310 @@
+# subgroup_auxilliary.py
+
+# The methods in this module should have very well-defined tasks
+# They should not call any methods in the main subgroup module
+# They should not take a diffuse parameter called "args", all parameters should have names
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import models as Models
+
+## General
+
+class Subgroup_collection:
+	# N is number of features in dataset
+	def __init__(self,N,models):
+		self.N = N
+		self.models = []
+		self.candidates = []
+		self.remaining = []
+		self.add(models,"MODELS")
+
+	def get_covered(self):
+		established = flatten([mod.subgroup for mod in self.models])
+		candidate = flatten([mod.subgroup for mod in self.candidates])
+		return established + candidate
+
+	def get_remaining(self):
+		covered = self.get_covered()
+		return [i for i in range(self.N) if i not in covered]
+
+	def add(self,mods,status):
+		covered = self.get_covered()
+		for mod in mods:
+			for feature in mod.subgroup:
+				if feature in covered:
+					print("Feature already covered by collection!!")
+					raise
+				else:
+					covered.append(feature)
+			if status == "MODELS":
+				self.models.append(mod)
+			elif status == "CANDIDATES":
+				self.candidates.append(mod)
+			elif status == "REMAINING":
+				self.remaining.append(mod)
+
+	def reset(self):
+		self.candidates = []
+		self.remaining = []
+
+# turns list of lists into list
+def flatten(lst):
+	return [elem for sublist in lst for elem in sublist]
+
+def is_tensor(X,order):
+	#print(X)
+	#print(np.shape(X))
+	if sum(np.array(np.shape(X)) > 1) == order:
+		return True
+	else:
+		return False
+
+# it is decided that this should always work
+def num_features(dat):
+	if is_tensor(dat,1):
+		return 1
+	else:
+		return np.shape(dat)[1]
+
+## Candidate generation
+
+def map_idx(arr,idx):
+	return [arr[i] for i in idx]
+
+def print_mat(mat):
+	print()
+	for row in mat:
+		print("".join(["{0:.3f}".format(elem).rjust(10,' ') for elem in row]))
+
+'''
+def forgiving_inv(arr):
+	inverse = 1/arr
+	return inverse
+	
+	for i,inv in enumerate(inverse):
+		if not np.isfinite(inv):
+			inverse[i] = 0
+	return inverse
+	
+	#return np.array([1/elem for elem in arr if elem else 0])
+'''
+
+def normalize_corr_mat(dep):
+	if is_tensor(dep,2):
+		#autoprecisions = forgiving_inv(np.diag(dep))
+		autocorr = np.sqrt(np.diag(1/np.diag(dep)))
+		#print_mat(autocorr)
+		return np.dot(autocorr,np.dot(dep,autocorr))
+	elif is_tensor(dep,3):
+		autocorr = np.sqrt(np.diag(1/np.diag(dep[0,:,:])))
+		for i in range(np.shape(dep)[0]):
+			dep[i,:,:] = np.dot(autocorr,np.dot(dep[i,:,:],autocorr))
+		return dep
+		
+def linear_dependence(dat,lag):
+	N = num_features(dat)
+	dep = np.dot(dat.T,dat)
+	#print_mat(dep)
+	for i in range(1,lag):
+		dep += np.dot(dat[i:,:].T,dat[:-i,:])
+
+	#dep = normalize_corr_mat(dep)
+	#print_mat(dep)
+	return dep
+
+def autocorr(dat,max_ord):
+	N = num_features(dat)
+	T = np.shape(dat)[0]
+
+	y = np.zeros([max_ord,N,N])
+	y[0,:,:] = np.dot(dat.T,dat)
+	for i in range(1,max_ord):
+		y[i,:,:] = (np.dot(dat[i:,:].T,dat[:-i,:]) + np.dot(dat[:-i,:].T,dat[i:,:]))/2
+	
+	return y/T
+
+def whiteness_test(data,explanations,max_ord=40):
+	N = num_features(data[0])
+	
+	acorr = sum([autocorr(dat,max_ord) for dat in data])
+	acorr = normalize_corr_mat(acorr)
+	T = sum([np.shape(dat)[0] for dat in data])
+	for i in range(N):
+		for j in range(i):
+			plt.figure()
+			plt.title("Cross-ACF for {0:s} \n and {1:s}".format(explanations[i],explanations[j]))
+			plt.xlabel("order")
+			plt.plot(acorr[:,i,j])
+			plt.plot([0,max_ord],[2/np.sqrt(T)]*2,'r')
+			plt.plot([0,max_ord],[-2/np.sqrt(T)]*2,'r')
+			plt.legend(["acf","whiteness"])
+	plt.show()
+
+def proportional_random_select(arr):
+	c_arr = np.cumsum(arr)
+	#print(c_arr)
+	c_arr = c_arr/c_arr[-1]
+	
+	u = np.random.random()
+	i = 0
+	while u > c_arr[i]:
+		i += 1
+
+	return i
+
+def greedy_random_cand(dep,num):
+	dep = np.abs(dep)
+	for i in range(len(dep)):
+		dep[i,i] = 0
+
+	idx0 = proportional_random_select(np.sum(dep,axis=0))
+	#print(idx0)
+	idxs = [idx0]
+	for i in range(1,num):
+		#print_mat(dep)
+		#print()
+		dep_sum = np.sum(dep[idxs,:],axis=0)
+		selected = proportional_random_select(dep_sum)
+		#print(selected)
+		for idx in idxs:
+			dep[selected,idx] = 0
+			dep[idx,selected] = 0
+
+		idxs.append(selected)
+
+	#print_mat(dep)
+	#print()
+
+	return idxs
+
+## Training
+
+def train_varma(data,subgroup,p,q,re_series,rw_series):
+	N = num_features(data[0])
+	print(N)
+	#p = args.settings["VARMA_p"]
+	#q = args.settings["VARMA_q"]
+
+	A_hist = np.zeros([1,N,N*p])
+	C_hist = np.zeros([1,N,N*q])
+
+	orders = [N,p,q]
+	mod = Models.VARMA(orders)
+	i = 0
+	for dat in data:
+		if is_tensor(dat,1):
+			dat = np.reshape(dat,[len(dat),1])
+
+		mod.reset()
+		for y in dat[:p,:]:
+			mod.update(y)
+		A_h,C_h = mod.annealing(dat[p:,:],re_series,rw_series,initiate= i == 0) #initiate = i==0
+		if i >= 0:
+			A_hist = np.concatenate([A_hist,A_h[-5:]],axis=0)
+			C_hist = np.concatenate([C_hist,C_h[-5:]],axis=0)
+		i += 1
+	mod.reset()
+
+	A_hist = A_hist[1:]
+	C_hist = C_hist[1:]
+
+	#plot_train_varma(A_hist,C_hist,N,p,q)
+	A = np.mean(A_hist,axis=0)
+	C = np.mean(C_hist,axis=0)
+	#print_mat(mod.A)
+	#print_mat(mod.C)
+	print_mat(A)
+	print_mat(C)
+
+
+	#gt_A = -np.array([[0.1,0.5001,0.2],[-0.2,0.1,0.5001],[0.5001,0.2,-0.1]])
+	#print("Incremental, error: " + str(np.linalg.norm(gt_A-mod.A[:3,:3])))
+	#print("Averaging, error: " + str(np.linalg.norm(gt_A-A[:3,:3])))
+	
+	mod.set_A_C(A,C)
+
+	mod.subgroup = subgroup
+
+	return mod
+
+def plot_train_varma(A_hist,C_hist,k,p,q):
+	for j in range(k):
+		plt.figure()
+		plt.title("AR coefficients for y_{0:d}".format(j+1))
+		for i in range(k*p):
+			plt.plot(A_hist[:,j,i])
+		#for i in range(k*p):
+		#	plt.plot([0,N],[-A[j][i]]*2)
+		legends = flatten([["a({0:d},{1:d})".format(jx+1,ix+1) for jx in range(k)] for ix in range(p)]) # + flatten([["a({0:d},{1:d})_gt".format(jx+1,ix+1) for ix in range(p)] for jx in range(k)])
+		#print([["a({0:d},{1:d})_gt".format(jx+1,ix+1) for ix in range(p)] for jx in range(k)])
+		plt.legend(legends) #["a{0:d}".format(i+1) for i in range(k*p)]+["a{0:d}_gt".format(i+1) for i in range(p)])
+
+	if q:
+		plt.figure()
+		plt.title("MA coefficients")
+		for i in range(q):
+			plt.plot(C_hist[:,0,i])
+		#for i in range(q):
+		#	plt.plot([0,N],[C[0][i+1]]*2)
+		plt.legend(["c{0:d}".format(i+1) for i in range(q)]) #+["c{0:d}_gt".format(i+1) for i in range(q)])
+
+	plt.show()
+
+def impending_failure(data,names,dataset,failure_horizon):
+	if dataset == "TURBOFAN":
+		for dat in data:
+			X,y = impending_failure_datapoints(dat,True,failure_horizon)
+			yield X,y
+	elif dataset == "BACKBLAZE":
+		for dat,name in zip(data,names):
+			failure = "_fail" in name
+			X,y = impending_failure_datapoints(dat,failure,failure_horizon)
+			yield X,y
+
+def impending_failure_datapoints(dat,failure,failure_horizon):
+	N,M = dat.shape
+	if failure:
+		neg_X = dat[:-failure_horizon,:]
+		pos_X = dat[-failure_horizon:,:]
+		X = np.concatenate([neg_X,pos_X])
+		y = np.concatenate([-np.ones([N-failure_horizon,1]), np.ones([failure_horizon,1])])
+		#w = np.concatenate([np.ones([N-failure_horizon,1]), self.pos_w*np.ones([failure_horizon,1])])
+	else:
+		X = dat[:-failure_horizon,:]
+		y = -np.ones([N-failure_horizon,1])
+		#w = np.ones([N-failure_horizon,1])
+	return X,y
+
+'''
+def remaining_features(N,models):
+	remains = list(range(N))
+	for mod in models:
+		for feature in mod.subgroup:
+			remains.remove(feature)
+	return remains
+'''
+
+## Testing
+
+def reset_models(models):
+	for mod in models:
+		mod.reset()
+
+def update_models(data,models):
+	for mod in models:
+		mod.update_array(data[:,mod.subgroup])
+
+def predict_data(dat,models,k):
+	pred_mat = np.zeros_like(dat)
+	i = 0
+	for sample in dat:
+		pred_array = np.zeros_like(sample)
+		for mod in models:
+			mod.update(sample[mod.subgroup])
+			pred_array[mod.subgroup] = mod.predict(k)
+
+		pred_mat[i,:] = pred_array
+		i += 1
+	return pred_mat
---
+++ svmtest.py
@@ -0,0 +1,42 @@
+#svmtest.py
+
+import numpy as np 
+import sklearn.svm as svm
+import matplotlib.pyplot as plt
+
+def main():
+	# create data
+	a_length = 10
+	b_length = 1000
+	a = np.random.normal([5,3],1,[a_length,2])
+	b = np.random.normal([0,0],4,[b_length,2])
+
+	plt.figure(0)
+	plt.title("Ground truth")
+	plt.scatter(a[:,0],a[:,1],color='r')
+	plt.scatter(b[:,0],b[:,1],color='b')
+	plt.legend(["negative","positive"],loc=2)
+
+	X = np.concatenate([a,b])
+	y = np.concatenate([-np.ones([a_length,1]),np.ones([b_length,1])])
+	w = np.ravel(np.concatenate([10*np.ones([a_length,1]),np.ones([b_length,1])]))
+	# train SVM
+	mod = svm.SVC()
+
+	mod.fit(X,y,w)
+
+	# test
+	y_hat = mod.predict(X)
+
+	# evaluate
+	'''
+	plt.figure(1)
+	plt.title("Predicted")
+	plt.scatter(X[y_hat==-1,0],X[y_hat==-1,1],color='r')
+	plt.scatter(X[y_hat==1,0],X[y_hat==1,1],color='b')
+	'''
+
+	plt.show()
+
+if __name__ == '__main__':
+	main()
